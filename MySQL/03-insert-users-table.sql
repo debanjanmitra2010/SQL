e31570b705079f4bf6d07ab2f5c3cec3ca51637f
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-01-30 22:13:56 -0500

    Delete 03-insert-users-table.sql

--- MySQL/03-insert-users-table.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (yearly_salary , full_name, current_status) VALUES (19000, 'Justin Bond', 'self-employed');
-INSERT INTO users (yearly_salary , full_name, current_status) VALUES (0, 'Steve Martin', 'unemployed');
-INSERT INTO users (yearly_salary , full_name, current_status) VALUES (25000, 'Anne Wright', 'employed');
