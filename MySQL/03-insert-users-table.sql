0ccd0d371acab70d0d0494bf8c4dd5f6cfa73fad
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-01-30 22:03:02 -0500

    Add files via upload

---
+++ MySQL/03-insert-users-table.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (yearly_salary , full_name, current_status) VALUES (19000, 'Justin Bond', 'self-employed');
+INSERT INTO users (yearly_salary , full_name, current_status) VALUES (0, 'Steve Martin', 'unemployed');
+INSERT INTO users (yearly_salary , full_name, current_status) VALUES (25000, 'Anne Wright', 'employed');
