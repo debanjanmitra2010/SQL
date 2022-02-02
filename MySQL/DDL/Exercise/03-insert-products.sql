191cf70f72def357555fcb34a788ddacd97f2103
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-02-01 20:08:11 -0500

    Add files via upload

---
+++ MySQL/DDL/Exercise/03-insert-products.sql
@@ -0,0 +1,10 @@
+INSERT INTO products (product_name, price, product_description, stock, image_path)
+VALUES ('Speaker', 568.98, 'Incredible loud speaker', 20, 'D:speaker/speaker.jpg');
+INSERT INTO products (product_name, price, product_description, stock, image_path)
+VALUES ('Monitor', 856.98, 'Excellent Display', 40, 'D:monitor/monitor.jpg');
+INSERT INTO products (product_name, price, product_description, stock, image_path)
+VALUES ('Laptop', 986.98, 'Super Fast Laptop', 80, 'D:laptop/laptop.jpg');
+INSERT INTO products (product_name, price, product_description, stock, image_path)
+VALUES ('Mouse', 88.98, 'Great Quality', 800, 'D:mouse/mouse.jpg');
+INSERT INTO products (product_name, price, product_description, stock, image_path)
+VALUES ('Mobile', 599.98, 'New Gen Smartphone', 520, 'D:mobile/mobile.jpg');
