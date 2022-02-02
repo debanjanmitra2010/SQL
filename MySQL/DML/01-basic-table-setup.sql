55029c8c1f361ab59bca6a65591a9bb650d3daff
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-02-01 20:09:33 -0500

    Create 01-basic-table-setup.sql

---
+++ MySQL/DML/01-basic-table-setup.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE sales_example;
+CREATE TABLE sales(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    date_create DATE DEFAULT (CURRENT_DATE),
+    date_fulfilled DATE,
+    customer_name VARCHAR(300) NOT NULL,
+    product_name VARCHAR(300) NOT NULL,
+    volume NUMERIC(10,3) NOT NULL CHECK(volume >=0),
+    is_recurring BOOLEAN DEFAULT FALSE,
+    is_disputed BOOLEAN DEFAULT FALSE
+);
