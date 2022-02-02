d0ff54839d37238c59066999ff0960f4acc28d1f
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-02-01 20:05:45 -0500

    Add files via upload

---
+++ MySQL/DDL/08-insert-conversations-table.sql
@@ -0,0 +1,6 @@
+INSERT INTO conversations (user_name, employer_name, message, date_send)
+VALUES('Justin Bond', 'Apple', 'Hi, I like Coding', '2022-01-15 16:32:10' );
+INSERT INTO conversations (user_name, employer_name, message, date_send)
+VALUES('Steve Martin','Google', 'I am a Tech Enthu', '2022-01-15 16:33:11' );
+INSERT INTO conversations (user_name, employer_name, message, date_send)
+VALUES('Anne Wright','FaceBook', 'Hi, I am smart', '2022-01-15 16:34:12' );
---
+++ MySQL/DDL/09-select-all-tables.sql
@@ -0,0 +1,1 @@
+SELECT * FROM employers e INNER JOIN conversations c ON e.company_name = c.employer_name;
