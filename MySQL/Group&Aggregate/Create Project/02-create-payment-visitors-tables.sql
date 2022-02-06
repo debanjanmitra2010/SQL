eca0c4a07b13b66976a19e8986a653289e1fe239
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-02-05 22:18:03 -0500

    Add files via upload

---
+++ MySQL/Group&Aggregate/Create Project/02-create-payment-visitors-tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE payment_methods (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(200)
+);
+
+CREATE TABLE tables (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    num_seats INT,
+    category VARCHAR(200)
+);
