343bf8385c74f4944d1698d24e10899a93b70e8b
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-02-07 23:36:52 -0500

    Create 03-add-additional-customer.sql

---
+++ MySQL/Database Transaction/create-project/03-add-additional-customer.sql
@@ -0,0 +1,18 @@
+INSERT INTO customers(
+    first_name,
+    last_name,
+    email
+)
+VALUES(
+    'Jenifer',
+    'Jones',
+    'Jenifer@Jones.com'
+);
+
+INSERT INTO orders(
+   amount_billed,
+   customer_id 
+)
+VALUES(
+    103.12
+);
